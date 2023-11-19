23e593635e5cf68be1384917a93fa0073bf35caf
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2023-11-19 11:55:42 +0900

    initial commit

---
+++ main.cc
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <opencv2/opencv.hpp>
+
+int main(int argc, char *argv[]) {
+  cv::Mat img = cv::imread("aaa.png", -1);
+  if (img.empty()) {
+    return -1;
+  }
+  cv::namedWindow("Example", cv::WINDOW_AUTOSIZE);
+  cv::imshow("Example", img);
+  cv::waitKey(0);
+  cv::destroyWindow("Example");
+  return 0;
+}
